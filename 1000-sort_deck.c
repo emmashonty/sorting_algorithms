eb4d2f49036c70f55b2aef32565b19c4d44a8589
Joseph Emmanuel <102731916+emmashonty@users.noreply.github.com>
2024-01-20 09:20:45 +0100

    Update 1000-sort_deck.c

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -1,60 +1,114 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "deck.h"
-
-void print_deck(const deck_node_t *deck)
+#include <stdio.h>
+/**
+ *_strcmp - compare two strings
+ *@str1: string
+ *@str2: string
+ *Return: 1 str1 and str2 is equal, 0 they are not equal
+ */
+int _strcmp(const char *str1, char *str2)
 {
-    size_t i;
-    char kinds[4] = {'S', 'H', 'C', 'D'};
+	size_t i = 0;
 
-    i = 0;
-    while (deck)
-    {
-        if (i)
-            printf(", ");
-        printf("{%s, %c}", deck->card->value, kinds[deck->card->kind]);
-        if (i == 12)
-            printf("\n");
-        i = (i + 1) % 13;
-        deck = deck->next;
-    }
+	if (str1 == '\0')
+		return (0);
+	while (str1[i])
+	{
+		if (str1[i] != str2[i])
+			return (0);
+		i++;
+	}
+	if (str1[i] == '\0' && str2[i])
+		return (0);
+	return (1);
+}
+/**
+ * get_card_position - return the position based on card you put in
+ * @node: represent the card
+ * Return: return the card position
+ */
+int get_card_position(deck_node_t *node)
+{
+	int value;
+
+	value = (*node).card->value[0] - '0';
+	if (value < 50 || value > 57)
+	{
+		if (_strcmp((*node).card->value, "Ace") == 1)
+			value = 1;
+		else if (_strcmp((*node).card->value, "10") == 1)
+			value = 10;
+		else if (_strcmp((*node).card->value, "Jack") == 1)
+			value = 11;
+		else if (_strcmp((*node).card->value, "Queen") == 1)
+			value = 12;
+		else if (_strcmp((*node).card->value, "King") == 1)
+			value = 13;
+	}
+	value += (*node).card->kind * 13;
+	return (value);
+}
+/**
+ *swap_card - swap a card for his previous one
+ *@card: card
+ *@deck: card deck
+ *Return: return a pointer to a card which was enter it
+ */
+deck_node_t *swap_card(deck_node_t *card, deck_node_t **deck)
+{
+	deck_node_t *back = card->prev, *current = card;
+	/*NULL, 19, 48, 9, 71, 13, NULL*/
+
+	back->next = current->next;
+	if (current->next)
+		current->next->prev = back;
+	current->next = back;
+	current->prev = back->prev;
+	back->prev = current;
+	if (current->prev)
+		current->prev->next = current;
+	else
+		*deck = current;
+	return (current);
 }
 
-deck_node_t *init_deck(const card_t cards[52])
+/**
+ * insertion_sort_deck - function that sorts a doubly linked deck
+ * of integers in ascending order using the Insertion sort algorithm
+ * @deck: Dobule linked deck to sort
+ */
+void insertion_sort_deck(deck_node_t **deck)
 {
-    deck_node_t *deck;
-    deck_node_t *node;
-    size_t i;
+	int value_prev, value_current;
+	deck_node_t *node;
 
-    i = 52;
-    deck = NULL;
-    while (i--)
-    {
-        node = malloc(sizeof(*node));
-        if (!node)
-            return (NULL);
-        node->card = &cards[i];
-        node->next = deck;
-        node->prev = NULL;
-        if (deck)
-            deck->prev = node;
-        deck = node;
-    }
-    return (deck);
+	if (deck == NULL || (*deck)->next == NULL)
+		return;
+	node = (*deck)->next;
+	while (node)
+	{
+		/* preparing the previous value */
+		if (node->prev)
+		{
+			value_prev = get_card_position((node->prev));
+			value_current = get_card_position(node);
+		}
+		while ((node->prev) && (value_prev > value_current))
+		{
+			value_prev = get_card_position((node->prev));
+			value_current = get_card_position(node);
+			node = swap_card(node, deck);
+
+		}
+		node = node->next;
+	}
 }
-
-int main(void)
+/**
+ * sort_deck - sort a deck you put in using
+ * insertion sort algorithm
+ * @deck: deck
+ */
+void sort_deck(deck_node_t **deck)
 {
-    card_t cards[52] = {
-        {"Jack", CLUB}, {"4", HEART}, {"3", HEART}, {"3", DIAMOND}, {"Queen", HEART}, {"5", HEART}, {"5", SPADE}, {"10", HEART}, {"6", HEART}, {"5", DIAMOND}, {"6", SPADE}, {"9", HEART}, {"7", DIAMOND}, {"Jack", SPADE}, {"Ace", DIAMOND}, {"9", CLUB}, {"Jack", DIAMOND}, {"7", SPADE}, {"King", DIAMOND}, {"10", CLUB}, {"King", SPADE}, {"8", CLUB}, {"9", SPADE}, {"6", CLUB}, {"Ace", CLUB}, {"3", SPADE}, {"8", SPADE}, {"9", DIAMOND}, {"2", HEART}, {"4", DIAMOND}, {"6", DIAMOND}, {"3", CLUB}, {"Queen", CLUB}, {"10", SPADE}, {"8", DIAMOND}, {"8", HEART}, {"Ace", SPADE}, {"Jack", HEART}, {"2", CLUB}, {"4", SPADE}, {"2", SPADE}, {"2", DIAMOND}, {"King", CLUB}, {"Queen", SPADE}, {"Queen", DIAMOND}, {"7", CLUB}, {"7", HEART}, {"5", CLUB}, {"10", DIAMOND}, {"4", CLUB}, {"King", HEART}, {"Ace", HEART},
-    };
-    deck_node_t *deck;
-
-    deck = init_deck(cards);
-    print_deck(deck);
-    printf("\n");
-    sort_deck(&deck);
-    printf("\n");
-    print_deck(deck);
-    return (0);
+	insertion_sort_deck(deck);
 }
